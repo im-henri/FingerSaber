0d8d92b566abcca9f224e757aefa416524ef903a
im-henri <haidakhenri@gmail.com>
2021-09-14 13:20:55 +0300

    Startup crash fix. Now compining with inline-hook

--- buildQMOD.ps1
+++ buildQMOD.ps1
@@ -6,8 +6,8 @@ if (-not ($PSVersionTable.PSEdition -eq "Core")) {
     $buildScript += ".cmd"
 }
 
-$ArchiveName = "FingerSaber_v0.1.0.qmod"
-$TempArchiveName = "FingerSaber_v0.1.0.qmod.zip"
+$ArchiveName = "FingerSaber_v0.1.1.qmod"
+$TempArchiveName = "FingerSaber_v0.1.1.qmod.zip"
 
 & $buildScript NDK_PROJECT_PATH=$PSScriptRoot APP_BUILD_SCRIPT=$PSScriptRoot/Android.mk NDK_APPLICATION_MK=$PSScriptRoot/Application.mk
 Compress-Archive -Path "./libs/arm64-v8a/libFingerSaber.so", "./libs/arm64-v8a/libbeatsaber-hook_2_3_0.so", "./mod.json", "./Cover.jpg" -DestinationPath $TempArchiveName -Force
