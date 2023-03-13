dd4043c33605e7f43c5f166ca416b267df7fb1b7
Putra Iskandar <125776987+piskndar@users.noreply.github.com>
2023-03-14 07:02:22 +0800

    Update SOME_controller_hooks.cpp

--- src/hooks/SOME_controller_hooks.cpp
+++ src/hooks/SOME_controller_hooks.cpp
@@ -1,7 +1,7 @@
 #include "FingerSaber.hpp"
 #include "main.hpp"
 
-#include "Config.hpp"
+#include "ModConfig.hpp"
 
 #include "GlobalNamespace/OVRInput.hpp"
 #include "GlobalNamespace/OVRInput_Button.hpp"
