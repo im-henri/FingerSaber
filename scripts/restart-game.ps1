36697b514e632ded9e8ea640cc2b966e5ff11447
Alex Uskov <FrozenAlex@users.noreply.github.com>
2024-08-02 00:14:04 +0400

    Update

--- scripts/restart-game.ps1
+++ scripts/restart-game.ps1
@@ -1,4 +1,2 @@
 adb shell am force-stop com.beatgames.beatsaber
 adb shell am start com.beatgames.beatsaber/com.unity3d.player.UnityPlayerActivity
-Start-Sleep 1
-adb shell am start com.beatgames.beatsaber/com.unity3d.player.UnityPlayerActivity
